fbb051562cfccfed049bb165ef152908bf0d60f8
クレイ <83293295+KureiLe@users.noreply.github.com>
2022-07-07 22:15:59 +0700

    sddd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import disnake
 from disnake.ext import commands
+from disnake.ext.commands import has_permissions, MissingPermissions
 from datetime import datetime
 import validators
 import json
@@ -23,15 +24,11 @@ async def on_ready():
     await bot.change_presence(activity=disnake.Game(name="Touhou TD"), status="prefix = s!")
     print("I am online")
 
-@bot.command()
-async def help(ctx):
-    await ctx.send("hi")
-
 @bot.event
 async def on_message(message):
     if message.channel.id == RPchannel_ID:
         if message.author.id == bot.user.id:  
-            if message.content != "Proof has to be a link":
+            if message.content != "Lady Suwako only allow Admins to use this command." and message.content != "Please insert a link for the proof!" and message.content != "I'm sorry, but Lady Kanako forbids anyone from doing this command in this channel. Do it in <#{RPchannel_ID}> instead.":
                 await message.add_reaction("✅")
                 await message.add_reaction("❎")
         else:
@@ -80,6 +77,36 @@ async def on_raw_reaction_add(reaction):
         elif reaction.emoji.name != "✅" or "❎":
             await message.clear_reaction(emoji=reaction.emoji.name)
 
+
+
+
+
+
+
+@bot.command()
+async def help(ctx):
+    embed=disnake.Embed(title="Commands (Prefix = s!)", color=0x4fe64c)
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/988442510026235914/994600993574637689/d74ca0dcdcea3cd99cb9a93bb2a670ff.png")
+    embed.add_field(name="/reportbug", value="Report bug (need link as proof", inline=False)
+    embed.add_field(name="s!bugs or /bugs", value="View list of bugs sorted from most ✅", inline=False)
+    embed.add_field(name="s!delreport or /delreport (only visible for admins)", value="Delete report (argumment has to be the report message ID, find it or look for it in the s!bugs command)", inline=True)
+    await ctx.send(embed=embed)
+
+@bot.slash_command(description="Isn't it self explanatory")
+async def help(inter):
+    embed=disnake.Embed(title="Commands (Prefix = s!)", color=0x4fe64c)
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/988442510026235914/994600993574637689/d74ca0dcdcea3cd99cb9a93bb2a670ff.png")
+    embed.add_field(name="/reportbug", value="Report bug (need link as proof)", inline=False)
+    embed.add_field(name="s!bugs or /bugs", value="View list of bugs sorted from most ✅", inline=False)
+    embed.add_field(name="s!delreport or /delreport (only visible for admins)", value="Delete report (argumment has to be the report message ID, find it or look for it in the s!bugs command)", inline=True)
+    await inter.response.send_message(embed=embed)
+
+
+
+
+
+
+
 @bot.slash_command(description="Report a bug")
 async def reportbug(inter, bug: str, proof: str):
     if inter.channel_id == RPchannel_ID:
@@ -89,7 +116,7 @@ async def reportbug(inter, bug: str, proof: str):
 
         # Check if proof is acceptable
         if not validators.url(proof):
-            await inter.response.send_message("Proof has to be a link")
+            await inter.response.send_message("Please insert a link for the proof!")
         else:
             # Data
             data = {"reporter": inter.author.id, "time": current_time, "bug": bug, "proof": f"{proof} "}
@@ -105,7 +132,13 @@ async def reportbug(inter, bug: str, proof: str):
             data2 = {"reportMSG_ID": announce.id, "databaseMSG_ID": DBsend.id, "reactions": 0}
             await DBchannel2.send(str(data2))
     else:
-        await inter.response.send_message("Must report bug in the correct channel")
+        await inter.response.send_message(f"I'm sorry, but Lady Kanako forbids anyone from doing this command in this channel. Do it in <#{RPchannel_ID}> instead.")
+
+
+
+
+
+
 
 @bot.command()
 async def bugs(ctx):
@@ -124,18 +157,134 @@ async def bugs(ctx):
     DB2data_sorted = sorted(DB2data, key=lambda i: i["sortby"], reverse=True)
 
     #put in array
-    embed=disnake.Embed(title="Bug Reports", color=0x4fe64c)
-    for data in DB2data:
+    embed=disnake.Embed(title="Bug Reports", description="From most reactions to least", color=0x4fe64c)
+    for data in DB2data_sorted:
         dataDB2 = data["data"]
         # get database content from database1
         dataDB1_message = await DBchannel.fetch_message(dataDB2["databaseMSG_ID"])
         dataDB1 = json.loads(dataDB1_message.content.replace("'", '"'))
         # Gather info and boom assemble
         thebug = dataDB1["bug"]
+        thereaction = dataDB2["reactions"]
         thetime = dataDB1["time"]
         thereportid = dataDB2["databaseMSG_ID"]
         theproof = dataDB1["proof"]
-        embed.add_field(name=f"title: {thebug}\ntime: {thetime}", value=f"report_id: {thereportid}\nproof: {theproof}", inline=False)
+        embed.add_field(name=f"Title: {thebug} ({thereaction} reactions)\nTime: {thetime} (GMT+7)", value=f"Report_id: {thereportid}\nProof: {theproof}", inline=False)
     await ctx.send(embed=embed)
+
+@bot.slash_command(description="Get bug reports")
+async def bugs(inter):
+    if not inter.channel_id == RPchannel_ID:
+        # Get channels
+        DBchannel = bot.get_channel(DBchannel_ID)
+        DBchannel2 = bot.get_channel(DBchannel2_ID)
+
+        # Put DB2 data to array
+        DB2data = []
+        async for message in DBchannel2.history(limit=None):
+            message_data = json.loads(message.content.replace("'", '"'))
+            append_data = {"sortby": message_data["reactions"], "data": message_data}
+            DB2data.append(append_data)
+
+        # Sort the array
+        DB2data_sorted = sorted(DB2data, key=lambda i: i["sortby"], reverse=True)
+
+        #put in array
+        embed=disnake.Embed(title="Bug Reports", description="From most reactions to least", color=0x4fe64c)
+        embed.set_thumbnail(url="https://media.discordapp.net/attachments/988442510026235914/994600993574637689/d74ca0dcdcea3cd99cb9a93bb2a670ff.png")
+        for data in DB2data_sorted:
+            dataDB2 = data["data"]
+            # get database content from database1
+            dataDB1_message = await DBchannel.fetch_message(dataDB2["databaseMSG_ID"])
+            dataDB1 = json.loads(dataDB1_message.content.replace("'", '"'))
+            # Gather info and boom assemble
+            thebug = dataDB1["bug"]
+            thereaction = dataDB2["reactions"]
+            thetime = dataDB1["time"]
+            thereportid = dataDB2["databaseMSG_ID"]
+            theproof = dataDB1["proof"]
+            embed.add_field(name=f"Title: {thebug} ({thereaction} reactions)\nTime: {thetime} (GMT+7)", value=f"Report_id: {thereportid}\nProof: {theproof}", inline=False)
+        await inter.response.send_message(embed=embed)
+    else:
+        await inter.response.send_message(f"I'm sorry, but Lady Kanako forbids anyone from doing this command in this channel. Do it in <#{RPchannel_ID}> instead.")
+
+
+
+
+
+
+@bot.command(pass_context=True, name="delreport")
+@commands.has_permissions(administrator=True)
+async def delreport(ctx, message=None):
+    if not ctx.channel.id == RPchannel_ID:
+        if not message == None:
+            # Get channels
+            RPchannel = bot.get_channel(RPchannel_ID)
+            DBchannel = bot.get_channel(DBchannel_ID)
+            DBchannel2 = bot.get_channel(DBchannel2_ID)
+            
+            # Check if the ID is valid
+            isExist = False
+            async for ID in DBchannel2.history(limit=None):
+                dataID = json.loads(ID.content.replace("'", '"'))
+                if str(message) == str(dataID["reportMSG_ID"]):
+                    isExist = True
+
+            if isExist == False:
+                await ctx.send("Please insert the report ID (or bug report message ID)!")
+            else:
+                # Get info
+                async for x in DBchannel2.history(limit=None):
+                    data = json.loads(x.content.replace("'", '"'))
+                    if str(message) == str(data["reportMSG_ID"]):
+                        # DELETE
+                        RPmsg = await RPchannel.fetch_message(data["reportMSG_ID"])
+                        DBmsg =  await DBchannel.fetch_message(data["databaseMSG_ID"])
+                        await RPmsg.delete()
+                        await DBmsg.delete()
+                        await x.delete()
+
+                        await ctx.send("Done.")
+        else:
+            await ctx.send("Please insert the report ID (or bug report message ID)!")
+    else:
+        await ctx.send("I'm sorry, but Lady Kanako forbids anyone from doing this command in this channel. Do it in <#{RPchannel_ID}> instead.")
+
+@delreport.error
+async def delreport_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send(f"Lady Suwako only allow Admins to use this command.")
+
+
+@bot.slash_command(description="Delete report", name="delreport")
+@commands.default_member_permissions(administrator=True)
+async def delreport(inter, id: str):
+    # Get channels
+    RPchannel = bot.get_channel(RPchannel_ID)
+    DBchannel = bot.get_channel(DBchannel_ID)
+    DBchannel2 = bot.get_channel(DBchannel2_ID)
+    
+    # Check if the ID is valid
+    isExist = False
+    async for ID in DBchannel2.history(limit=None):
+        dataID = json.loads(ID.content.replace("'", '"'))
+        if str(id) == str(dataID["reportMSG_ID"]):
+            isExist = True
+
+    if isExist == False:
+        await inter.response.send_message("Please insert the report ID (or bug report message ID)!")
+    else:
+        # Get info
+        async for x in DBchannel2.history(limit=None):
+            data = json.loads(x.content.replace("'", '"'))
+            if str(id) == str(data["reportMSG_ID"]):
+                # DELETE
+                RPmsg = await RPchannel.fetch_message(data["reportMSG_ID"])
+                DBmsg =  await DBchannel.fetch_message(data["databaseMSG_ID"])
+                await RPmsg.delete()
+                await DBmsg.delete()
+                await x.delete()
+
+                await inter.response.send_message("Done.")
 
 bot.run(token)
